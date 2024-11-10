05e2101be5fc2cdb93e035e9869aba4706f31091
HTsulfuric <leo.phoenix2003@gmail.com>
2024-11-10 16:46:55 +0900

    20241110

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,3 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
@@ -16,3 +14,10 @@ vim.filetype.add {
     ["~/%.config/foo/.*"] = "fooscript",
   },
 }
+
+-- setup for rust-analyzer
+local bufnr = vim.api.nvim_get_current_buf()
+vim.keymap.set("n", "<leader>a", function()
+  vim.cmd.RustLsp "codeAction" -- supports rust-analyzer's grouping
+  -- or vim.lsp.buf.codeAction() if you don't want grouping.
+end, { silent = true, buffer = bufnr })
